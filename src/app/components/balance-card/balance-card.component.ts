9f251efc6e5e2fdba2a5011b6005f65c3e74e100
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-08-11 19:03:49 -0300

    Remove valores variáveis do componente

--- src/app/components/balance-card/balance-card.component.ts
+++ src/app/components/balance-card/balance-card.component.ts
@@ -5,13 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './balance-card.component.html',
   styleUrl: './balance-card.component.scss'
 })
-export class BalanceCardComponent {
-  totalBalanceValue = `R$ ${100},00`;
-  ccValue = `R$ ${200},00`;
-  linkedBalanceCC = `R$ ${300},00`;
-  linkedBalanceSA = `R$ ${400},00`;
-  automaticWithdrawal = `R$ ${500},00`;
-  noAutomaticWithdrawal = `R$ ${500},00`;
-  totalBalance = `R$ ${1000},00`;
-  freeMovement = `R$ ${1000},00`;
-}
+export class BalanceCardComponent { }
